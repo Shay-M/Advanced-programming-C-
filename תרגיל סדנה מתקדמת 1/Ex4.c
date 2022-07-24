eb92e4657920944db073f82980b016d095db9c34
shay <22634623+Shay-M@users.noreply.github.com>
2022-07-24 11:48:35 +0300

    Update Ex4.c

--- תרגיל סדנה מתקדמת 1/Ex4.c
+++ תרגיל סדנה מתקדמת 1/Ex4.c
@@ -1,6 +1,6 @@
 /*This Project was the result of blood sweat and tears of:
-Shay Mualem ID: 200332435  |Grop num 661108-62
-Maxim Gromov ID: 317225332 |Grop num 61108-61
+Shay Mualem   |Grop num 661108-62
+Maxim Gromov |Grop num 61108-61
 */
 
 #include<stdio.h>
